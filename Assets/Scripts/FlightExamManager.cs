72e8795d23ca267e4689eb33bba1af522c85d8a7
Kadir ADIMUTLU <kadiradimutlu@gmail.com>
2026-04-03 21:29:37 +0300

    feat: implement warning audio feedback in danger zone

--- Assets/Scripts/FlightExamManager.cs
+++ Assets/Scripts/FlightExamManager.cs
@@ -4,17 +4,23 @@ using TMPro;
 public class FlightExamManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text statusText;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip warningClip;
     
     private bool hasTakenOff = false;
     private bool threatCleared = false;
     private bool missionComplete = false;
+
+    public bool HasTakenOff() => hasTakenOff;
+    public bool IsThreatCleared() => threatCleared;
+    public bool IsMissionComplete() => missionComplete;
 
     public void ReportTakeoff()
     {
         if (!hasTakenOff)
         {
             hasTakenOff = true;
-            Debug.Log("Mission State: Aircraft has successfully taken off.");
+            Debug.Log("Mission State: Takeoff reported.");
         }
     }
 
@@ -25,15 +31,16 @@ public class FlightExamManager : MonoBehaviour
             statusText.text = "Entered a Dangerous Zone!";
             statusText.color = Color.red;
         }
+
+        if (audioSource != null && warningClip != null)
+        {
+            audioSource.PlayOneShot(warningClip);
+        }
     }
 
     public void ExitDangerZone()
     {
-        if (statusText != null)
-        {
-            statusText.text = ""; 
-        }
-        
+        if (statusText != null) statusText.text = ""; 
         threatCleared = true;
     }
 }
